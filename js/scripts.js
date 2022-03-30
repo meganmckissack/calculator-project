da0fcbb350f8b2f2735809bcdb0a96be35a7038f
Megan McKissack <meganmckissack@gmail.com>
2022-03-29 20:14:12 -0700

    Changes number intput to integer

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function add(number1, number2){
   return number1 + number2;
 }
 
-const number1 = prompt("Enter a number:");
-const number2 = prompt("Enter another number:");
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
 
 alert(add(number1, number2));
